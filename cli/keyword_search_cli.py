14111b49311c7533578e07a0a34ee2cd9fce1242
Aditya <mirageaditya@gmail.com>
2025-12-16 23:22:04 +0545

    feat: Add full BM25 search

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import argparse
-from services.processing import search_field, tokenize
+
 from services.indexing import InvertedIndex
+from services.processing import search_field, tokenize
+from services.utils import BM25_B, BM25_K1
 
 
 def main() -> None:
@@ -11,7 +13,7 @@ def main() -> None:
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
     search_keyword_parser = subparsers.add_parser(
-        "search_keyword", help="Search movies using BM25"
+        "search_keyword", help="Search movies using simple search"
     )
     search_keyword_parser.add_argument("query", type=str, help="Search query")
 
@@ -31,6 +33,31 @@ def main() -> None:
     tfidf_parser.add_argument("doc_id", type=int, help="Document id to search")
     tfidf_parser.add_argument("term", type=str, help="Term to search")
 
+    bm25_idf_parser = subparsers.add_parser(
+        "bm25idf", help="Get BM25 IDF score for a given term"
+    )
+    bm25_idf_parser.add_argument("term", type=str, help="Term to search")
+
+    bm25_tf_parser = subparsers.add_parser(
+        "bm25tf", help="Get BM25 TF score for a given document id and term"
+    )
+    bm25_tf_parser.add_argument("doc_id", type=int, help="Document id to search")
+    bm25_tf_parser.add_argument("term", type=str, help="Term to search")
+    bm25_tf_parser.add_argument(
+        "k1", type=float, nargs="?", default=BM25_K1, help="Turntable BM25 K1 parameter"
+    )
+    bm25_tf_parser.add_argument(
+        "b", type=float, nargs="?", default=BM25_B, help="Turntable BM25 b parameter"
+    )
+
+    bm25_search_parser = subparsers.add_parser(
+        "bm25search", help="Search movies using full BM25 scoring"
+    )
+    bm25_search_parser.add_argument("query", type=str, help="Search query")
+    bm25_search_parser.add_argument(
+        "--limit", type=int, nargs="?", default=5, help="Max results"
+    )
+
     subparsers.add_parser("build", help="Build inverted indexes from movies list")
 
     args = parser.parse_args()
@@ -41,7 +68,7 @@ def main() -> None:
 
             max_results = 5 if len(search_results) >= 5 else len(search_results)
             for i in range(max_results):
-                print(f"{i+1}. {search_results[i]['title']}")
+                print(f"{i + 1}. {search_results[i]['title']}")
 
         case "search":
             inverted_index = InvertedIndex()
@@ -58,7 +85,48 @@ def main() -> None:
                 print(f"No results found for: {args.query}")
             else:
                 for n, doc in enumerate(matching_docs):
-                    print(f"{n+1}. {doc['id']} {doc['title']}")
+                    print(f"{n + 1}. {doc['id']} {doc['title']}")
+
+        case "bm25search":
+            inverted_index = InvertedIndex()
+            try:
+                inverted_index.load()
+            except FileNotFoundError as e:
+                print("Error: ", e)
+                exit(1)
+
+            results = inverted_index.bm25_search(args.query, args.limit)
+            for idx, id in enumerate(results):
+                document = inverted_index.docmap[id]
+                title = document["title"]
+                score = results[id]
+                print(f"{idx + 1}. ({id}) {title} - Score: {score:.2f}")
+
+        case "bm25tf":
+            inverted_index = InvertedIndex()
+            try:
+                inverted_index.load()
+            except FileNotFoundError as e:
+                print("Error: ", e)
+                exit(1)
+
+            bm25tf = inverted_index.get_bm25_tf(
+                doc_id=args.doc_id, term=args.term, k1=args.k1, b=args.b
+            )
+            print(
+                f"BM25 TF score of {args.term} in document {args.doc_id}: {bm25tf:.2f}"
+            )
+
+        case "bm25idf":
+            inverted_index = InvertedIndex()
+            try:
+                inverted_index.load()
+            except FileNotFoundError as e:
+                print("Error: ", e)
+                exit(1)
+
+            bm25idf = inverted_index.get_bm25_idf(args.term)
+            print(f"BM25 IDF score of {args.term}: {bm25idf:.2f}")
 
         case "tfidf":
             inverted_index = InvertedIndex()
--- services/indexing.py
+++ services/indexing.py
@@ -1,10 +1,16 @@
-from typing import Dict, List
-from services.processing import tokenize, load_movies
-from pickle import dump, load
-from dotenv import load_dotenv
-from collections import Counter
-import os
 import math
+import os
+from collections import Counter
+from doctest import DocFileSuite
+from pickle import dump, load
+from pydoc import doc
+from typing import Dict, List
+
+from dotenv import load_dotenv
+
+from services.processing import load_movies, tokenize
+
+from .utils import BM25_B, BM25_K1
 
 load_dotenv()
 
@@ -12,6 +18,7 @@ cache_dir = os.environ["CACHE_DIR"]
 index_dir = f"{cache_dir}/index.pkl"
 docmap_dir = f"{cache_dir}/docmap.pkl"
 tf_dir = f"{cache_dir}/term_frequencies.pkl"
+doc_lengths_dir = f"{cache_dir}/doc_lengths.pkl"
 
 
 class InvertedIndex:
@@ -19,6 +26,7 @@ class InvertedIndex:
         self.index: Dict[str, set] = {}
         self.docmap: Dict[int, Dict] = {}
         self.term_frequencies: dict[int, Counter] = {}
+        self.docs_length: Dict[int, int] = {}
 
     def __add_document(self, doc_id: int, text: str):
         text_tks = tokenize(text)
@@ -26,6 +34,7 @@ class InvertedIndex:
             self.index.setdefault(tk, set()).add(doc_id)
 
         self.term_frequencies[doc_id] = Counter(text_tks)
+        self.docs_length[doc_id] = len(text_tks)
 
     def get_documents(self, term: str) -> List[int]:
         term = term.lower()
@@ -51,15 +60,76 @@ class InvertedIndex:
         doc_counter = self.term_frequencies.get(doc_id, Counter())
         return doc_counter.get(token, 0)
 
-    def get_idf(self, term: str) -> int:
+    def get_idf(self, term: str) -> float:
         tokens = tokenize(term)
         if len(tokens) > 1:
             raise ValueError("term must be a single word!")
 
         token = tokens[0]
+        n_docs = len(self.docmap)
         n_matching_docs = len(self.get_documents(token))
-        idf = math.log((len(self.docmap) + 1) / (n_matching_docs + 1))
+        idf = math.log((n_docs + 1) / (n_matching_docs + 1))
         return idf
+
+    def bm25(self, doc_id: int, term: str) -> float:
+        tokens = tokenize(term)
+        if len(tokens) > 1:
+            raise ValueError("term must be a single word!")
+
+        token = tokens[0]
+        bm25_tf = self.get_bm25_tf(doc_id, token)
+        bm25_idf = self.get_bm25_idf(token)
+
+        return bm25_tf * bm25_idf
+
+    def bm25_search(self, query: str, limit: int) -> Dict[int, float]:
+        tokens = tokenize(query)
+        scores: Dict[int, float] = {}
+        for token in tokens:
+            doc_ids = self.get_documents(token)
+            for doc_id in doc_ids:
+                scores[doc_id] = scores.get(doc_id, 0.0) + self.bm25(doc_id, token)
+
+        sorted_scores = sorted(
+            scores.items(),
+            key=lambda item: item[1],
+            reverse=True,
+        )
+        return dict(sorted_scores[:limit])
+
+    def get_bm25_tf(self, doc_id: int, term: str, k1=BM25_K1, b=BM25_B) -> float:
+        raw_tf = self.get_tf(doc_id, term)
+
+        doc_length = self.docs_length.get(doc_id, 0)
+        avg_doc_length = self.__get_avg_doc_length()
+
+        length_norm = 1 - b + b * (doc_length / avg_doc_length)
+
+        bm25_tf = (raw_tf * (k1 + 1)) / (raw_tf + k1 * length_norm)
+        return bm25_tf
+
+    def get_bm25_idf(self, term: str) -> float:
+        tokens = tokenize(term)
+        if len(tokens) > 1:
+            raise ValueError("term must be a single word!")
+
+        n_docs = len(self.docmap)
+
+        token = tokens[0]
+        n_matching_docs = len(self.get_documents(token))
+
+        bm25_idf = math.log(
+            (n_docs - n_matching_docs + 0.5) / (n_matching_docs + 0.5) + 1
+        )
+        return bm25_idf
+
+    def __get_avg_doc_length(self) -> float:
+        if not self.docs_length:
+            return 0.0
+
+        total_doc_length = sum(self.docs_length.values())
+        avg_doc_length = total_doc_length / len(self.docs_length)
+        return avg_doc_length
 
     def build(self):
         movies = load_movies()
@@ -84,16 +154,14 @@ class InvertedIndex:
         with open(tf_dir, "wb") as f:
             dump(self.term_frequencies, f)
 
+        with open(doc_lengths_dir, "wb") as f:
+            dump(self.docs_length, f)
+
     def load(self):
-
-        if not os.path.exists(index_dir):
-            raise FileNotFoundError(f"Could not find {index_dir}")
-
-        if not os.path.exists(docmap_dir):
-            raise FileNotFoundError(f"Could not find {docmap_dir}")
-
-        if not os.path.exists(tf_dir):
-            raise FileNotFoundError(f"Could not find {tf_dir}")
+        dirs = [index_dir, docmap_dir, tf_dir, doc_lengths_dir]
+        for dir in dirs:
+            if not os.path.exists(dir):
+                raise FileNotFoundError(f"Could not find {dir}")
 
         with open(index_dir, "rb") as f:
             self.index = load(f)
@@ -103,3 +171,6 @@ class InvertedIndex:
 
         with open(tf_dir, "rb") as f:
             self.term_frequencies = load(f)
+
+        with open(doc_lengths_dir, "rb") as f:
+            self.docs_length = load(f)
